8eb629c37a6d502567266bc414db929560e292ea
alessio darmanin <alessiodarmanin@alessios-iMac.local>
2019-02-05 09:52:27 +0100

    changed to 2019-01-01_15-30 and commecnted out image display

--- video/pi0/opencv_check_frame_similarity-FrameRegion_by_FrameRegion_v0.1_py.py
+++ video/pi0/opencv_check_frame_similarity-FrameRegion_by_FrameRegion_v0.1_py.py
@@ -96,21 +96,21 @@ def compare_images(imageA, imageB, title):
 	s = measure.compare_ssim(imageA, imageB)
  
 	# setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+	#fig = plt.figure(title)
+	#plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
  
 	# show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
+	#ax = fig.add_subplot(1, 2, 1)
+	#plt.imshow(imageA, cmap = plt.cm.gray)
+	#plt.axis("off")
  
 	# show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
+	#ax = fig.add_subplot(1, 2, 2)
+	#plt.imshow(imageB, cmap = plt.cm.gray)
+	#plt.axis("off")
  
 	# show the images
-	plt.show()
+	#plt.show()
 	return s
 
 for x in range(len(file_content_sorted)):
