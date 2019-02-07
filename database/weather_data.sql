ff8cf1972d3c4a848f0572ef4f2b57e31990880d
alessio darmanin <alessiodarmanin@alessios-iMac.local>
2019-02-07 22:17:06 +0100

    added WIP background subtractor

--- database/weather_data.sql
+++ database/weather_data.sql
@@ -12,11 +12,11 @@ FROM MScDB.dbo.weather_data
 order by filename desc
 ;
 
-SELECT Id, filename, [date], cast(SUBSTRING(filename,22,2)+':'+SUBSTRING(filename,24,2) as TIME) as file_time, [time], conditions, temperature, wind, UVIndex, rain, heatStress, sunrise, feelsLike, sunset, gustBft, seaTemp, humidity, pressure
+/*SELECT Id, filename, [date], cast(SUBSTRING(filename,22,2)+':'+SUBSTRING(filename,24,2) as TIME) as file_time, [time], conditions, temperature, wind, UVIndex, rain, heatStress, sunrise, feelsLike, sunset, gustBft, seaTemp, humidity, pressure
 FROM MScDB.dbo.weather_data
 where SUBSTRING(filename,14,8) = '20190204'
 order by filename desc
-;
+;*/
 
 SELECT *
 FROM MScDB.dbo.weather_data
